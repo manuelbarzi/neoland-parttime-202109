c5c93776f04a19a7f5545e33171f963806753042
manuelbarzi <manuelbarzi@gmail.com>
2021-10-08 21:14:06 +0200

    add more js challenges

--- staff/manuel-barzi/web/js/main.js
+++ staff/manuel-barzi/web/js/main.js
@@ -4,7 +4,7 @@ var z = y
 
 console.log('DEMO add')
 
-var add = function(a, b) {
+var add = function (a, b) {
     return a + b
 }
 
@@ -20,7 +20,7 @@ if (lang === 'en') {
     console.log('Hola, Mundo!')
 } else if (lang === 'it') {
     console.log('Ciao, Mondo!')
-} else if(lang === 'ru') {
+} else if (lang === 'ru') {
     console.log('Privet, Mir!')
 }
 
@@ -38,7 +38,7 @@ else if (age < 30)
     console.log('young adult')
 else if (age < 60)
     console.log('mature adult')
-else 
+else
     console.log('old adult')
 
 console.log('DEMO fruits')
@@ -90,7 +90,7 @@ console.log('DEMO spell (function)')
 function spell(text) {
     for (var i = 0; i < text.length; i++) {
         var char = text[i]
-    
+
         console.log(char)
     }
 }
@@ -124,9 +124,102 @@ console.log(spaces) // 1
 var spaces = countSpaces('adios mundo cruel')
 console.log(spaces) // 2
 
-var spaces = countSpaces('flipo con js, me encanta! es chuli piruli, chachi guai')
+var spaces = countSpaces('flipo con js, me encanta! es chachi piruli, chuli chili guai')
 console.log(spaces) // 8
 
 console.log('DEMO countVocals')
 
-// TODO
+function countVocals(text) {
+    // TODO
+}
+
+var vocals = countVocals('hola mundo')
+console.log(vocals) // 4
+
+var vocals = countVocals('adios mundo cruel')
+console.log(vocals) // 7
+
+var vocals = countVocals('pasito a pasito, suave, suavecito')
+console.log(vocals) // 15
+
+console.log('DEMO countPositiveNumbers')
+
+function countPositiveNumbers(numbers) {
+    // TODO
+}
+
+var positives = countPositiveNumbers([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+console.log(positives) // 9
+
+var positives = countPositiveNumbers([234, -897, 562, -958, 951])
+console.log(positives) // 3
+
+var positives = countPositiveNumbers([-101, 2, 13, 20, 21, -35, 80])
+console.log(positives) // 5
+
+console.log('DEMO countNegativeNumbers')
+
+function countNegativeNumbers(numbers) {
+    // TODO
+}
+
+var negatives = countNegativeNumbers([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+console.log(negatives) // 0
+
+var negatives = countNegativeNumbers([234, -897, 562, -958, 951])
+console.log(negatives) // 2
+
+var negatives = countNegativeNumbers([-101, 2, 13, 20, 21, -35, 80])
+console.log(negatives) // 2
+
+console.log('DEMO countEvenNumbers')
+
+function countEvenNumbers(numbers) {
+    // TODO
+}
+
+var evens = countEvenNumbers([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+console.log(evens) // 5
+
+var evens = countEvenNumbers([234, 897, 562, 958, 951])
+console.log(evens) // 3
+
+var evens = countEvenNumbers([-101, 2, 13, -20, 21, -35, 80])
+console.log(evens) // 3
+
+console.log('DEMO countOddNumbers')
+
+function countOddNumbers(numbers) {
+    // TODO
+}
+
+var odds = countOddNumbers([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+console.log(odds) // 5
+
+var odds = countOddNumbers([234, 897, 562, 958, 951])
+console.log(odds) // 2
+
+var odds = countOddNumbers([-101, 2, 13, -20, 21, -35, 80])
+console.log(odds) // 4
+
+console.log('DEMO countNumbers')
+
+function countNumbers(numbers, what) {
+    // TODO
+}
+
+var positives = countNumbers([-101, 2, 13, -20, 0, 21, -35, 80], 'positive')
+console.log(positives) // 4
+
+var negatives = countNumbers([-101, 2, 13, 0, -20, 21, -35, 80], 'negative')
+console.log(negatives) // 3
+
+var evens = countNumbers([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 'even')
+console.log(evens) // 5
+
+var odds = countNumbers([234, 897, 562, 958, 951], 'odd')
+console.log(odds) // 2
+
+var all = countNumbers([-101, 2, 13, -20, 21, -35, 80])
+console.log(all) // 7
+
