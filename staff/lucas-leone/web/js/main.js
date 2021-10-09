bfd6385dea479808195c2118a4f609203b8b8899
Lucas Leone <lucasmati9@gmail.com>
2021-10-09 20:46:43 +0200

    js homework complete

--- staff/lucas-leone/web/js/main.js
+++ staff/lucas-leone/web/js/main.js
@@ -131,7 +131,6 @@ console.log('DEMO countVocals')
 
 // TODO
 
-
 function countVocals(text) {
     var count = 0
 
@@ -170,3 +169,141 @@ function countVocals(text) {
     }
     return count
 }
+
+var vocals = countVocals('hola mundo')
+console.log(vocals) // 4
+
+var vocals = countVocals('adios mundo cruel')
+console.log(vocals) // 7
+
+var vocals = countVocals('pasito a pasito, suave, suavecito')
+console.log(vocals) // 15
+
+console.log('DEMO countPositiveNumbers')
+
+
+function countPositiveNumbers(numbers) {
+    var count=0
+    for (var i =0;i<numbers.length;i++) {
+        var num =numbers[i]
+        if (num > 0) {
+            count++
+        }
+    }
+    return count
+}
+var vocals = countVocals('hola mundo')
+console.log(vocals) // 4
+
+var vocals = countVocals('adios mundo cruel')
+console.log(vocals) // 7
+
+var vocals = countVocals('pasito a pasito, suave, suavecito')
+console.log(vocals) // 15
+
+console.log('DEMO countNegativeNumbers')
+
+function countNegativeNumbers(numbers){
+    var count=0
+    for (var i =0;i<numbers.length;i++) {
+        var num =numbers[i]
+        if (num < 0) {
+            count++
+        }
+    }
+    return count
+
+}
+
+var negatives = countNegativeNumbers([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+console.log(negatives) // 0
+
+var negatives = countNegativeNumbers([234, -897, 562, -958, 951])
+console.log(negatives) // 2
+
+var negatives = countNegativeNumbers([-101, 2, 13, 20, 21, -35, 80])
+console.log(negatives) // 2
+
+console.log('DEMO countEvenNumbers')
+
+function countEvenNumbers(numbers) {
+
+    var count = 0
+
+    for (var i=0;i<numbers.length;i++){
+        var num= numbers[i]
+        if (num==0){
+            count++
+        }else if (num%2==0){
+            count++
+        }
+        
+    }
+    return count
+}
+
+var evens = countEvenNumbers([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+console.log(evens) // 5
+
+var evens = countEvenNumbers([234, 897, 562, 958, 951])
+console.log(evens) // 3
+
+var evens = countEvenNumbers([-101, 2, 13, -20, 21, -35, 80])
+console.log(evens) // 3
+
+console.log('DEMO countOddNumbers')
+
+function countOddNumbers(numbers) {
+    var count = 0
+
+    for (var i=0;i<numbers.length;i++){
+        var num= numbers[i]
+        if (num%2!=0){
+            count++
+        }
+        
+    }
+    return count
+}
+    
+var odds = countOddNumbers([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+console.log(odds) // 5
+
+var odds = countOddNumbers([234, 897, 562, 958, 951])
+console.log(odds) // 2
+
+var odds = countOddNumbers([-101, 2, 13, -20, 21, -35, 80])
+console.log(odds) // 4
+
+console.log('DEMO countNumbers')
+
+function countNumbers(numbers, what) {
+    if (what=='positive'){
+      var result =  countPositiveNumbers(numbers)
+    }else if (what=="negative"){
+        var result = countNegativeNumbers(numbers)
+    }else if (what=="even"){
+        var result = countEvenNumbers(numbers)
+    }
+    else if (what=="odd"){
+        var result = countOddNumbers(numbers)
+    }else {
+        var result= numbers.length
+    }
+    return result
+}
+
+var positives = countNumbers([-101, 2, 13, -20, 0, 21, -35, 80], 'positive')
+console.log(positives) // 4
+
+var negatives = countNumbers([-101, 2, 13, 0, -20, 21, -35, 80], 'negative')
+console.log(negatives) // 3
+
+var evens = countNumbers([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 'even')
+console.log(evens) // 5
+
+var odds = countNumbers([234, 897, 562, 958, 951], 'odd')
+console.log(odds) // 2
+
+var all = countNumbers([-101, 2, 13, -20, 21, -35, 80])
+console.log(all) // 7
