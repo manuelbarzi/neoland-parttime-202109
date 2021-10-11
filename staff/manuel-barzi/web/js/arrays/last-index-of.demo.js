f7865f93e50153f29ed01f333c356febf4ccad52
manuelbarzi <manuelbarzi@gmail.com>
2021-10-11 20:58:43 +0200

    add more js challenges

---
+++ staff/manuel-barzi/web/js/arrays/last-index-of.demo.js
@@ -0,0 +1,13 @@
+console.log('DEMO lastIndexOf')
+
+// DEMO 1
+
+var array = ['ant', 'bison', 'camel', 'duck', 'camel', 'bison']
+var index = lastIndexOf(array, 'camel')
+console.log(index) // 4
+
+// DEMO 2
+
+var array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var index = lastIndexOf(array, 3)
+console.log(index) // 13
