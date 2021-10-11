4987dd27716361d0242e02b0bb29a59c3d1976d7
manuelbarzi <manuelbarzi@gmail.com>
2021-10-11 20:29:18 +0200

    add new strings and arrays challenges

---
+++ staff/manuel-barzi/web/js/arrays/index-of.demo.js
@@ -0,0 +1,13 @@
+console.log('DEMO indexOf')
+
+// DEMO 1
+
+var array = ['ant', 'bison', 'camel', 'duck', 'bison']
+var index = indexOf(array, 'camel')
+console.log(index) // 2
+
+// DEMO 2
+
+var array = ['adios', 'mundo', 'cruel']
+var index = indexOf(array, 'cruel')
+console.log(index) // 2
