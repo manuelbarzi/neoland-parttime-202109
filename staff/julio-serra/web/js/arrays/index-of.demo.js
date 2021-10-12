e5417856c74e6ead71eb244763a285eda34e333c
codejules <julioserra@protonmail.com>
2021-10-12 20:17:13 +0200

    add my exercise of main.js

---
+++ staff/julio-serra/web/js/arrays/index-of.demo.js
@@ -0,0 +1,11 @@
+console.log('DEMO indexOf')
+
+// DEMO 1
+
+var index = indexOf('hola mundo', 'm')
+console.log(index) // 5
+
+// DEMO 2
+
+var index = indexOf('adios mundo cruel', 'c')
+console.log(index) // 12
---
+++ staff/julio-serra/web/js/arrays/reverse.js
@@ -0,0 +1,9 @@
+function reverse(reverted) {
+    // TODO
+ 
+    var text = 0;  
+
+    //saber las letras
+    // mostrarlas al reves
+
+}
