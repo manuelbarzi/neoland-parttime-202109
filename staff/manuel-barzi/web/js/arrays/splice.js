991b28cdb5f47ef3649f48f5b7a0af70aaf26221
manuelbarzi <manuelbarzi@gmail.com>
2021-10-14 20:47:45 +0200

    add splice test and first impl

--- staff/manuel-barzi/web/js/arrays/splice.js
+++ staff/manuel-barzi/web/js/arrays/splice.js
@@ -1,1 +1,13 @@
-// TODO
+function splice(array, start, deleteCount, item1) {
+    if (deleteCount === 0 && item1) {
+        for (var i = array.length; i > start; i--) {
+            array[i] = array[i - 1]
+        }
+
+        array[start] = item1
+
+        return []
+    } else if (deleteCount > 0) {
+        // TODO
+    }
+}
