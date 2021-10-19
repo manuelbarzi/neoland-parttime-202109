cf9f4842f23ff2dd9f01beb059f4810181d05659
codejules <julioserra@protonmail.com>
2021-10-19 23:12:54 +0200

    trying exercises js

--- staff/julio-serra/web/js/strings/includes.js
+++ staff/julio-serra/web/js/strings/includes.js
@@ -1,20 +1,45 @@
+
 function includes(phrase, search, position) {
-    var c = 0 // contador a 0
+    var c = 0
 
-    for (var i = 0; i < phrase.length; i++) {
+    // for (var i = 0; i < phrase.length; i++) {
 
+    for (var i = position? position : 0; i < phrase.length; i++) {
+        //variable para guardar cada iteracion de la frase
         var char = phrase[i]
-        var target = search[c]
 
-        if (char === target) {
+        //variable para guardar cada letra que busca y va guardando en el contador que inicia en 0
+        var element = search[c]
+
+        if (char === element) { // si el caracter es igual a cada letra que va guardando, y es sí, 
+            // que sume uno al contador para volver a iniciar
             c++
-            if (c === search.length) {
+            if (c === search.length) { // si el total del contador es igual a la longitud de lo que buscamos
                 return true
             }
         }
-        else {
-            c = 0
-        }
+        else
+        c = 0 // si es que no que ponga el contador a 0 de nuevo
     }
+
     return false
+
+    // var c = 0 // contador a 0
+    // for (var i = 0; i < phrase.length; i++) {
+
+    //     var char = phrase[i]
+    //     var target = search[c]
+
+    //     if (char === target) {
+    //         c++
+    //         if (c === search.length) {
+    //             return true
+    //         }
+    //     }
+    //     else {
+    //         c = 0
+    //     }
+    // }
+    // return false
+
 }
