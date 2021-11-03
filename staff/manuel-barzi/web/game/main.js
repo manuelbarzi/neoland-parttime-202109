ded5eb91b47f2cfd343fd018e8f7e1c68cfb3e7e
manuelbarzi <manuelbarzi@gmail.com>
2021-11-03 20:06:14 +0100

    boost game

--- staff/manuel-barzi/web/game/main.js
+++ staff/manuel-barzi/web/game/main.js
@@ -25,7 +25,8 @@ aster.y = 100
 
 asterImage.style.transform = translate(aster.x, aster.y)
 
-document.onkeydown = function (event) {
+//document.onkeydown = function (event) {
+document.addEventListener('keydown', function (event) {
     if (event.key === 'ArrowRight')
         ship.x += step // ship.x = ship.x + step
 
@@ -49,7 +50,8 @@ document.onkeydown = function (event) {
 
         console.log('collision! you have ' + points + ' points')
     }
-}
+    // }
+})
 
 var direction = 1
 
@@ -63,10 +65,118 @@ setInterval(function () {
     aster.x += direction * step
 
     asterImage.style.transform = translate(aster.x, aster.y)
-}, 500);
+}, 500)
 
 // utils
 
 function translate(x, y) {
     return 'translate(' + x + 'px, ' + y + 'px)'
 }
+
+// canvas
+
+var canvas = document.querySelector('.canvas')
+var ctx = canvas.getContext('2d')
+
+ctx.fillStyle = 'transparent'
+ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+function paintHappySmile() {
+    ctx.beginPath()
+    ctx.arc(canvas.width / 2, canvas.height / 2, 30, 0, 2 * Math.PI)
+    ctx.fillStyle = 'yellow'
+    ctx.fill()
+
+    ctx.beginPath()
+    ctx.arc(canvas.width / 3, canvas.height / 2, 5, 0, 2 * Math.PI)
+    ctx.fillStyle = 'black'
+    ctx.fill()
+
+    ctx.beginPath()
+    ctx.arc(canvas.width * 2 / 3, canvas.height / 2, 5, 0, 2 * Math.PI)
+    ctx.fillStyle = 'black'
+    ctx.fill()
+
+    ctx.beginPath()
+    ctx.arc(canvas.width / 2, canvas.height * 3 / 5, 10, 0, Math.PI)
+    ctx.strokeStyle = 'black'
+    ctx.lineWidth = 5
+    ctx.stroke();
+}
+
+function paintUnhappySmile() {
+    ctx.beginPath()
+    ctx.arc(canvas.width / 2, canvas.height / 2, 30, 0, 2 * Math.PI)
+    ctx.fillStyle = 'yellow'
+    ctx.fill()
+
+    ctx.beginPath()
+    ctx.arc(canvas.width / 3, canvas.height / 2, 5, 0, 2 * Math.PI)
+    ctx.fillStyle = 'black'
+    ctx.fill()
+
+    ctx.beginPath()
+    ctx.arc(canvas.width * 2 / 3, canvas.height / 2, 5, 0, 2 * Math.PI)
+    ctx.fillStyle = 'black'
+    ctx.fill()
+
+    ctx.beginPath()
+    ctx.arc(canvas.width / 2, canvas.height * 2 / 3, 10, Math.PI, 0)
+    ctx.strokeStyle = 'black'
+    ctx.lineWidth = 5
+    ctx.stroke();
+}
+
+paintHappySmile()
+
+var happy = true
+
+setInterval(function () {
+    if (happy)
+        paintUnhappySmile()
+    else
+        paintHappySmile()
+
+    happy = !happy
+
+}, 1000)
+
+var smile = {
+    x: 0,
+    y: 0,
+    width: 100,
+    height: 100
+}
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'd')
+        smile.x += step // smile.x = smile.x + step
+
+    if (event.key === 'a')
+        smile.x -= step
+
+    if (event.key === 's')
+        smile.y += step
+
+    if (event.key === 'w')
+        smile.y -= step
+
+    canvas.style.transform = translate(smile.x, smile.y)
+
+    if (smile.x + smile.width / 2 >= aster.x + aster.width * margin
+        && smile.x + smile.width / 2 <= aster.x + aster.width * (1 - margin)
+        && smile.y >= aster.y + aster.height * margin
+        && smile.y <= aster.y + aster.height * (1 - margin)) {
+
+        points++
+
+        console.log('collision! you have ' + points + ' points')
+    }
+})
+
+var smileImage = document.querySelector('.smile')
+
+smileImage.addEventListener('click', function () {
+    var audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3');
+    audio.play();
+})
