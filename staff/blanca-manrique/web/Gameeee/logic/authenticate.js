4edccc2eabea907676faaf912dce06bb96e5320c
Blanca Manrique <blancamanrique13@gmail.com>
2021-11-10 21:44:38 +0100

    add logic to game canvas version

---
+++ staff/blanca-manrique/web/Gameeee/logic/authenticate.js
@@ -0,0 +1,22 @@
+function authenticateUser(username, password, callback) {
+ var xhr = new XMLHttpRequest
+ xhr.open('POST','https://b00tc4mp.herokuapp.com/api/v2/users/auth')
+ xhr.addEventListener('load', function(){
+     if(this.status === 401){
+        var res = JSON.parse(this.responseText)
+        var error = res.error
+        callback(new Error(error))
+     }else if( this.status === 200){
+        return this.responseText
+     }
+ })
+    xhr.setRequestHeader('Content-type', 'application/json')
+
+    var data = {}
+
+    data.username = username
+    data.password = password
+    var json = JSON.stringify(data)
+
+    xhr.send(json)
+}
---
+++ staff/blanca-manrique/web/Gameeee/logic/register-user.js
@@ -0,0 +1,26 @@
+function registerUser(name, username, password, callback) {
+   var xhr = new XMLHttpRequest
+
+   xhr.open('POST', 'https://b00tc4mp.herokuapp.com/api/v2/users')
+   //Configuarar qué va a hacer cuando haya respuesta de la API
+   //'load' se dispara cuando hay respuesta de la API
+   xhr.addEventListener('load', function () {
+      if (this.status === 409) {
+         var res = JSON.parse(this.responseText) // me va a sacar un objeto
+
+         var error = res.error
+         callback(new Error(error)) //le ponemos dentro el mensaje de error
+      } else if(this.status === 201){
+         callback(null)
+         //Si todo va bien (201) no nos devuelve nada, como en Insomnia
+      }
+   })
+
+   //Le indico que le voy a enviar un json
+   xhr.setRequestHeader('Content-type', 'application/json')
+
+   //Construyo un json
+   var json = '{"name": "' + name + '", "username": "' + username + '", "password": "' + password + '"}'
+   // Envio el json
+   xhr.send(json)
+}
