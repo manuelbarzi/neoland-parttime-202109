4ea857a851ec6d0fc04087cbfaa113c49767d4fd
manuelbarzi <manuelbarzi@gmail.com>
2021-11-15 20:22:30 +0100

    add more stxff

--- staff/manuel-barzi/web/game/logic/register-user.js
+++ staff/manuel-barzi/web/game/logic/register-user.js
@@ -1,4 +1,16 @@
 function registerUser(name, username, password, callback) {
+    if (typeof name !== 'string') throw new TypeError(name + ' is not string')
+    if (!name.trim()) throw new Error('name is empty or blank')
+
+    if (typeof username !== 'string') throw new TypeError(username + ' is not string')
+    if (!username.trim()) throw new Error('username is empty or blank')
+
+    if (typeof password !== 'string') throw new TypeError(password + ' is not string')
+    if (!password.trim()) throw new Error('password is empty or blank')
+    if (password.trim().length < 8) throw new Error('password length is smaller than 8 characters')
+
+    if (typeof callback !== 'function') throw new TypeError(callback + ' is not a function')
+
     var xhr = new XMLHttpRequest
 
     xhr.open('POST', 'https://b00tc4mp.herokuapp.com/api/v2/users')
@@ -16,14 +28,8 @@ function registerUser(name, username, password, callback) {
     })
 
     xhr.setRequestHeader('Content-type', 'application/json')
-    
-    //var json = '{ "name": "' + name + '", "username": "' + username + '", "password": "' + password + '" }'
 
-    var data = {}
-
-    data.name = name
-    data.username = username
-    data.password = password
+    var data = { name: name, username: username, password: password }
 
     var json = JSON.stringify(data)
 
