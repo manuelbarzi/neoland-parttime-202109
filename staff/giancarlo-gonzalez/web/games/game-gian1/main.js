ab18e6e14f97edb2d747d9b62b8a61f06a4e8ea7
Giancarlo <gian_15_16@hotmail.com>
2021-11-18 21:28:22 +0100

    update game

---
+++ staff/giancarlo-gonzalez/web/games/game-gian1/main.js
@@ -0,0 +1,164 @@
+var signupPanel = document.querySelector('.signup')
+var postSignupPanel = document.querySelector('.post-signup')
+var signinPanel = document.querySelector('.signin')
+var gamePanel = document.querySelector('.game')
+
+var _token
+
+var signupSigninButton = signupPanel.querySelector('.signup__signin')
+
+signupSigninButton.addEventListener('click', function () {
+    signupPanel.classList.add('off')
+    signinPanel.classList.remove('off')
+})
+
+var signinSignupButton = signinPanel.querySelector('.signin__signup')
+
+signinSignupButton.addEventListener('click', function () {
+    signinPanel.classList.add('off')
+    signupPanel.classList.remove('off')
+})
+
+var signupForm = signupPanel.querySelector('form')
+
+signupForm.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    var nameInput = signupForm.name
+    var usernameInput = signupForm.username
+    var passwordInput = signupForm.password
+
+    var name = nameInput.value
+    var username = usernameInput.value
+    var password = passwordInput.value
+
+    registerUser(name, username, password, function (error) {
+        if (error) {
+            alert(error.message)
+
+            return
+        }
+
+        signupPanel.classList.add('off')
+        postSignupPanel.classList.remove('off')
+    })
+})
+
+var postSignupSigninButton = postSignupPanel.querySelector('button')
+
+postSignupSigninButton.addEventListener('click', function () {
+    postSignupPanel.classList.add('off')
+    signinPanel.classList.remove('off')
+})
+
+var signinForm = signinPanel.querySelector('form')
+
+signinForm.addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    var usernameInput = signinForm.username
+    var passwordInput = signinForm.password
+
+    var username = usernameInput.value
+    var password = passwordInput.value
+
+    authenticateUser(username, password, function (error, token) {
+        if (error) {
+            var siginFeedback = signinPanel.querySelector('.signin__feedback')
+
+            siginFeedback.innerText = error.message
+
+            siginFeedback.classList.remove('off')
+        } else {
+            retrieveUser(token, function (error, user) {
+                if (error) {
+                    var siginFeedback = signinPanel.querySelector('.signin__feedback')
+        
+                    siginFeedback.innerText = error.message
+        
+                    siginFeedback.classList.remove('off')
+
+                    return 
+                }
+
+                _token = token
+
+                var gameUser = gamePanel.querySelector('.game__user')
+
+                gameUser.innerText = 'Hello, ' + user.name + '!'
+
+                signinPanel.classList.add('off')
+                gamePanel.classList.remove('off')
+
+                start()
+            })
+        }
+    })
+
+})
+
+function start() {
+    var step = 10, margin = 0, points = 0
+
+    var bird = {
+        x: 0,
+        y: 0,
+        width: 100,
+        height: 100 * 369 / 423 
+    }
+
+    var pipe = {
+        x: 0,
+        y: 0,
+        width: 100,
+        height: 100 * 792 / 514// 100 * 792 / 514 // ~154
+    }
+
+    var pipeImage = document.querySelector('.pipe')
+
+    pipeImage.style.transform = translate(pipe.x,pipe.y)
+
+    var birdImage = document.querySelector('.bird')
+
+    bird.x = 100
+    bird.y = 100
+
+    birdImage.style.transform = translate(bird.x, bird.y)
+
+    //document.onkeydown = function (event) {
+    document.addEventListener('keydown', function (event) {
+        if (event.key === 'ArrowRight')
+            pipe.x += step // ship.x = ship.x + step
+
+        if (event.key === 'ArrowLeft')
+            pipe.x -= step
+
+        if (event.key === 'ArrowDown')
+            pipe.y += step
+
+        if (event.key === 'ArrowUp')
+            pipe.y -= step
+
+        pipeImage.style.transform = translate(pipe.x, pipe.y)
+
+        if (pipe.x + pipe.width / 2 >= bird.x + bird.width * margin
+            && pipe.x + pipe.width / 2 <= bird.x + bird.width * (1 - margin)
+            && pipe.y >= bird.y + bird.height * margin
+            && pipe.y <= bird.y + bird.height * (1 - margin)) {
+
+            points++
+
+            console.log('collision! you have ' + points + ' points')
+        }
+        // }
+    })
+
+    var direction = 1
+
+   
+    // utils
+
+    function translate(x, y) {
+        return 'translate(' + x + 'px, ' + y + 'px)'
+    }
+}
