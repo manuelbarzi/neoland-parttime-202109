efccdcca29a49464a8d4dae0ca268b84b5842d32
Juan Jose Ruiz Suarez <juanjoruiz@MacBook-Pro.local>
2021-11-24 21:13:20 +0100

    add staff app

---
+++ staff/juanjo-ruiz/front-end/demo-app/components/Register.jsx
@@ -0,0 +1,24 @@
+class Register extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {feedback : null}
+    } 
+
+    render() {
+        return <div>
+            <form>
+                <input type="text" name="name" placeholder="name" />
+                <input type="text" name="username" placeholder="username" />
+                <input type="password" name="password" placeholder="password" />
+                <button>Register</button>
+            </form>
+
+            <a href="" onClick={event => {
+                event.preventDefault()
+            
+                this.props.onLoginClick()
+            }}>Login</a>
+        </div>
+    }
+}
