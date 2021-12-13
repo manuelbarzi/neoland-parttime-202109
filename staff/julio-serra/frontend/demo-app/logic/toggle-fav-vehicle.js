e7836b23a13c5ac11e02c37a4369b337349364ef
codejules <julioserra@protonmail.com>
2021-12-13 19:49:08 +0100

    update toggle fav vehicle

--- staff/julio-serra/frontend/demo-app/logic/toggle-fav-vehicle.js
+++ staff/julio-serra/frontend/demo-app/logic/toggle-fav-vehicle.js
@@ -1,4 +1,4 @@
-function toogleFavVehicle(token, id, callback) {
+function toggleFavVehicle(token, id, callback) {
     if (typeof token !== 'string') throw new TypeError('token is not string')
     if (!token.trim()) throw new Error('token is empty or blank')
     if (token.split('.').length !== 3) throw new Error('invalid token')
@@ -12,7 +12,7 @@ function toogleFavVehicle(token, id, callback) {
 
     xhr.open('GET', 'https://b00tc4mp.herokuapp.com/api/v2/users')
 
-    xhr.onload(function () {
+    xhr.addEventListener('load', function() {
         if (this.status === 401) {
             const res = JSON.parse(this.responseText)
             const error = res.error
@@ -24,10 +24,45 @@ function toogleFavVehicle(token, id, callback) {
             const favs = user.favs || []
 
             const index = favs.indexOf(id)
+
+            if (index < 0)
+                favs.push(id)
+
+            else
+                favs.splice(index, 1)
+
+            const xhr = new XMLHttpRequest
+
+            xhr.open ('PATCH', 'https://b00tc4mp.herokuapp.com/api/v2/users')
+
+            xhr.addEventListener('load', function() {
+                if (this.status === 400 || this.status === 401 || this.status === 409) {
+                    const res = JSON.parse(this.responseText)
+
+                    const error = res.error
+
+                    callback(new Error(error))
+
+                } else if (this.status === 204) {
+                    callback(null)
+                }
+            })
+
+            xhr.setRequestHeader('Authorization', 'Bearer ' + token)
+            xhr.setRequestHeader('Content-type', 'aplication/json')
+
+            const data = { favs: favs }
+
+            const json = JSON.stringify(data)
+
+            xhr.send(json)
+
         }
     })
 
+    xhr.setRequestHeader('Authorization', 'Bearer ' + token)
 
+    xhr.send()
 
 
 }
