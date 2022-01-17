6ea9e1f718a8cad01f2f8973241d4dbf010bda6d
manuelbarzi <manuelbarzi@gmail.com>
2022-01-17 20:31:53 +0100

    add cart add, remove, retrieve logics

--- staff/manuel-barzi/front-end/demo-app/logic/toggle-fav-vehicle.js
+++ staff/manuel-barzi/front-end/demo-app/logic/toggle-fav-vehicle.js
@@ -1,11 +1,6 @@
 function toggleFavVehicle(token, id, callback) {
-    if (typeof token !== 'string') throw new TypeError('token is not string')
-    if (!token.trim()) throw new Error('token is empty or blank')
-    if (token.split('.').length !== 3) throw new Error('invalid token')
-
-    if (typeof id !== 'string') throw new TypeError('id is not string')
-    if (!id.trim()) throw new Error('id is empty or blank')
-
+    validateToken(token)
+    validateId(id)
     validateCallback(callback)
 
     const xhr = new XMLHttpRequest
