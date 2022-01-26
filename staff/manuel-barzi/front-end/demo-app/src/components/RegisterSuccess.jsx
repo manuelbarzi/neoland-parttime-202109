1dec75a94f6ac81ed52ef3dca04a7f6c277f5dd8
manuelbarzi <manuelbarzi@gmail.com>
2022-01-26 20:10:00 +0100

    initial migration to react-app

---
+++ staff/manuel-barzi/front-end/demo-app/src/components/RegisterSuccess.jsx
@@ -0,0 +1,13 @@
+function RegisterSuccess({ onLoginClick }) {
+    logger.debug('RegisterSuccess -> "render"')
+
+    const clickLogin = event => {
+        event.preventDefault()
+
+        onLoginClick()
+    }
+
+    return <p>User successfully register, you can proceed to <a href="" onClick={clickLogin}>Login</a>.</p>
+}
+
+export default RegisterSuccess
