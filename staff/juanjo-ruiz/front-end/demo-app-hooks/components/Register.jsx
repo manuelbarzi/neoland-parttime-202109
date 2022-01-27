a5d8979292a39c911c6f8ad7de79d74fcbb6bdb7
Juan Jose Ruiz Suarez <juanjoruiz@MacBook-Pro.local>
2022-01-27 22:44:27 +0100

    refactoring demo-app hooks and add react-demo-app

---
+++ staff/juanjo-ruiz/front-end/demo-app-hooks/components/Register.jsx
@@ -0,0 +1,53 @@
+const { useState } = React
+
+function Register({ onRegistered, onLoginClick }) {
+    const [feedback, setFeedback] = useState(null)
+
+    const onRegisterUser = event => {
+        event.preventDefault()
+
+        const name = event.target.name.value
+        const city = event.target.city.value
+        const username = event.target.username.value
+        const password = event.target.password.value
+
+        try {
+            registerUser(name, city, username, password, error => {
+                if (error) {
+                    setFeedback(error.message)
+
+                    return
+                }
+
+                onRegistered()
+
+            })
+        } catch (error) {
+            setFeedback(error.message)
+        }
+    }
+
+    const showLogin = event => {
+        event.preventDefault()
+
+        onLoginClick()
+    }
+
+    return <div className="container">
+        <form className="form form-container" onSubmit={onRegisterUser}>
+            <h2 className="title title-form">Registrate</h2>
+
+            <input className="input input-form" type="text" name="name" placeholder="nombre" requerid />
+            <input className="input input-form" type="text" name="city" placeholder="ciudad" requerid />
+            <input className="input input-form" type="text" name="username" placeholder="usuario" requerid />
+            <input className="input input-form" type="password" name="password" placeholder="contraseña" requerid />
+
+            <button className="button button-form">Registrate</button>
+
+            {feedback ? <p>{feedback}</p> : null}
+        </form>
+
+        <p><a href="" onClick={showLogin}>Inicia sesión</a> si ya estas registrado</p>
+    </div>
+}
+
---
+++ staff/juanjo-ruiz/front-end/demo-app-hooks/components/Search.jsx
@@ -0,0 +1,16 @@
+function Search(onQuery) {
+
+    const submit = event => {
+        event.preventDefault()
+
+        var query = event.target.query.value
+
+        onQuery(query)
+    }
+
+    return <form onSubmit={submit} >
+        <h4>Busca tu coche</h4>
+        <input className="input" type="text" name="query" placeholder="criterio" defaultValue={submit} />
+        <button className="button">Buscar</button>
+    </form>
+}
