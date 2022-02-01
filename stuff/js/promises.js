b1461409fc546b1073a3dcd1408498082c58e293
manuelbarzi <manuelbarzi@gmail.com>
2022-02-01 19:44:46 +0100

    add more promises demos

--- stuff/js/promises.js
+++ stuff/js/promises.js
@@ -1,19 +1,42 @@
-// setTimeout(() => {
-//     console.log('hola mundo')
-// }, 3000)
+new Promise((resolve, reject) => {
+    resolve(10)
+    //reject(new Error('chungo 1'))
+})
+    .then(num => console.log(num))
+    .catch(error => console.error(error))
+    .then(() => 20)
+    .then(num => console.log(num))
+    .then(() => {
+        //throw new Error('chungo 2')
 
-
-const setTimeoutPromised = millis => {
-    return new Promise((resolve, reject) => {
-        // setTimeout(() => {
-        //     resolve()
-        // }, millis)
-
-        setTimeout(resolve, millis)
-        //setTimeout(reject, millis)
+        return 30
     })
-}
-
-setTimeoutPromised(3000)
-    .then(() => console.log('hola mundo'))
-    .catch(() => console.log('adios mundo cruel'))
+    .then(num => console.log(num))
+    .catch(error => console.error(error))
+    .then(() => 
+        new Promise((resolve, reject) => 
+             //reject(new Error('chungo 3'))
+            resolve(31)
+        )
+    )
+    //.then(() => 40)
+    .catch(error => console.error(error))
+    .then(num => console.log(num))
+    .then(() => new Promise((resolve, reject) => 
+                        reject(new Error('chungo 4'))
+                    )
+                    .then(num => console.log(num))
+                    .catch(error => console.error(error))
+                    .then(() => 50)
+    )
+    .then(num => console.log(num))
+    .catch(error => console.error(error))
+//10
+//20
+//30
+//31
+//Error: chungo 4
+//   at <anonymous>:26:32
+//    at new Promise (<anonymous>)
+//    at <anonymous>:25:17
+//50
