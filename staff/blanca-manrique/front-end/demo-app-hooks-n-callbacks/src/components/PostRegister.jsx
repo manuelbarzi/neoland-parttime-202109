b40c4581c9557136bdc3d54b449481b5f5c5ee0a
Blanca Manrique <blancamanrique13@gmail.com>
2022-02-02 18:21:29 +0100

    demo app: hooks and callbacks

---
+++ staff/blanca-manrique/front-end/demo-app-hooks-n-callbacks/src/components/PostRegister.jsx
@@ -0,0 +1,17 @@
+import logger from '../logger'
+
+function PostRegister(onLoginClick) {
+    logger.debug('PostRegister -> render')
+
+    const goToLogin= event => {
+        event.preventDefault()
+        onLoginClick()
+    }
+    
+    return <div>
+        <h2>User successfully register</h2>
+        <button onClick={goToLogin}>Login</button>
+    </div>
+} 
+
+export default PostRegister
