67ff986af48b281d0f361355920203a1111367e7
manuelbarzi <manuelbarzi@gmail.com>
2022-02-02 19:42:31 +0100

    add more promises demos

--- stuff/js/promises.js
+++ stuff/js/promises.js
@@ -1,42 +1,12 @@
-new Promise((resolve, reject) => {
-    resolve(10)
-    //reject(new Error('chungo 1'))
-})
-    .then(num => console.log(num))
-    .catch(error => console.error(error))
-    .then(() => 20)
-    .then(num => console.log(num))
-    .then(() => {
-        //throw new Error('chungo 2')
-
-        return 30
-    })
-    .then(num => console.log(num))
-    .catch(error => console.error(error))
-    .then(() => 
-        new Promise((resolve, reject) => 
-             //reject(new Error('chungo 3'))
-            resolve(31)
-        )
-    )
-    //.then(() => 40)
-    .catch(error => console.error(error))
-    .then(num => console.log(num))
-    .then(() => new Promise((resolve, reject) => 
-                        reject(new Error('chungo 4'))
-                    )
-                    .then(num => console.log(num))
-                    .catch(error => console.error(error))
-                    .then(() => 50)
-    )
-    .then(num => console.log(num))
-    .catch(error => console.error(error))
-//10
-//20
-//30
-//31
-//Error: chungo 4
-//   at <anonymous>:26:32
-//    at new Promise (<anonymous>)
-//    at <anonymous>:25:17
-//50
+//new Promise((resolve, reject) => resolve(1))
+Promise.resolve(1)
+//new Promise((resolve, reject) => reject('wtf'))
+//Promise.reject('wtf')
+    //.then(val => console.log(val))
+    //.catch(error => console.error(error))
+    .then(() => fetch('https://b00tc4mp.herokuapp.com/api/hotwheels/vehicles?color=black').then(res => res.json()))
+    //.then(res => res.json())
+    //.then(vehicles => console.log(vehicles))
+    .then(vehicles => vehicles.map(vehicle => fetch(`https://b00tc4mp.herokuapp.com/api/hotwheels/vehicles/${vehicle.id}`).then(res => res.json())))
+    .then(fetches => Promise.all(fetches))
+    .then(details => details.forEach(detail => console.log(detail)))
