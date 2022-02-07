3c7cff0f3cdcd66fba10690afeba793aec794997
Julio Serra <julioserra@Mac-mini-de-Julio.local>
2022-02-07 19:26:09 +0100

    update components app and logout

--- staff/julio-serra/frontend/demo-app/src/components/App.jsx
+++ staff/julio-serra/frontend/demo-app/src/components/App.jsx
@@ -1,56 +1,56 @@
-import { useState } from "react"
-import Login from "./Login"
-import Register from "./Register"
-import PostRegister from "./PostRegister"
-import Home from "./Home"
-import { Routes, Route, useNavigate, Navigate } from "react-router-dom"
+import { useState } from 'react'
+import Login from './Login'
+import Register from './Register'
+import PostRegister  from './PostRegister'
+import Home from './Home'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 function App() {
-    const [view, setView] = useState(sessionStorage.token ? "home" : "login") //al cambiar de vista se añade View
+    const [view, setView] = useState(sessionStorage.token ? 'home' : 'login') //al cambiar de vista se añade View
     const [token, setToken] = useState(sessionStorage.token ? sessionStorage.token : null)
-    const navigate = useNavigate()
 
-    const onRegister = () => setView("register")
+const onRegister = () => setView('register')
 
-    const onLogin = (token) => {
-        setToken(token)
-        setView("home")
+const onLogin = token => {
+    setToken(token)
+    setView('home')
     }
-    const goToLogin = () => setView("login")
-
-    const logout = () => {
-        setToken(null)
-        setView("login")
-    }
+    
+const goLogin = () => setView('login')
 
 
-    return <Routes>
-        <Route path="*" element={token ? <Home token={token} onLoggedOut={logout} /> : <Navigate to="login" />} />
-        <Route path="login" element={!token ? <Login onRegisterClick={onRegister} onLoggedIn={onLogin} /> : <Navigate to="/" />} />
-        <Route path="register" element={!token ? (view !== 'register-success' ? <Register onLoginClick={goToLogin} onRegistered={onRegister} /> : <PostRegister onLoginClick={goToLogin} />) : <Navigate to="/" />} />
-    </Routes>
+const logOut = () => {
+    setView('login')
+    setToken(null)
+}
 
-    // if (view === 'login')
-    //     return <Login
-    //         onRegisterClick={onRegister}
-    //         onLoggedIn={onLogin}
-    //     />
 
-    // if (view === 'register')
-    //     return <Register
-    //         onLoginClick={goToLogin}
-    //         onRegistered={onRegister}
-    //     />
+return <Routes>
+<Route path="*" element={token ? <Home token={token} onLoggedOut={logOut} /> : <Navigate to="login" />} />
+<Route path="login" element={!token ? <Login onRegisterClick={onRegister} onLoggedIn={onLogin} /> : <Navigate to="/" />} />
+<Route path="register" element={!token ? (view !== 'register-success' ? <Register onLoginClick={goLogin} onRegistered={onRegister} /> : <PostRegister onLoginClick={goLogin} />) : <Navigate to="/" />} />
+</Routes>
 
-    // else if (view === 'PostRegister')
-    //     return <PostRegister
-    //         onLoginClick={goToLogin} />
+        // if (view === 'login')
+        //     return <Login
+        //         onRegisterClick={onRegister}
+        //         onLoggedIn={onLogin}
+        //     />
 
-    // else if (view === 'home')
-    //     return <Home token={token}
-    //         logOut={logout}
-    //     />
+        // else if (view === 'register')
+        //     return <Register
+        //         onLoginClick={goLogin}
+        //         onRegistered={onRegister}
+        //     />
 
+        // else if (view === 'PostRegister')
+        //     return <PostRegister
+        //         onLoginClick={goLogin} />
+
+        // else if (view === 'home')
+        //     return <Home token={token}
+        //     onLoggedOut={logOut}    
+        //     />
 }
 
 export default App
