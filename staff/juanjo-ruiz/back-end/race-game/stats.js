abdacbc04293ca50320390b8feb091ed2fb6b38f
Juan Jose Ruiz Suarez <juanjoruiz@MacBook-Pro.local>
2022-02-09 18:41:15 +0100

    add stats code

--- staff/juanjo-ruiz/back-end/race-game/stats.js
+++ staff/juanjo-ruiz/back-end/race-game/stats.js
@@ -5,8 +5,35 @@ const { readFile, readdir } = fs
 
 readdir('.')
     .then(files => {
+        const txts = files.filter(file => file.endsWith('.txt'))
 
+        const reads = txts.map(txt => readFile(txt, 'uft8'))
 
-        console.log(files)
+        return Promise.all(reads)
     })
-    .catch(console.log)
+    .then(races => races.reduce((stats, race) => {
+        const lines = race.split('\n')
+
+        stats['🚘'].avg += lines[0].length / races.length
+        stats['🚖'].avg += lines[1].length / races.length
+        stats['🚔'].avg += lines[2].length / races.length
+
+        const last = lines[lines.length - 1]
+
+        if (last.includes('🚘'))
+            stats['🚘'].wins++
+
+        if (last.includes('🚖'))
+            stats['🚖'].wins++
+
+        if (last.includes('🚔'))
+            stats['🚔'].wins++
+
+        return stats
+    }, {
+        '🚘': { wins: 0, avg: 0},
+        '🚖': { wins: 0, avg: 0},
+        '🚔': { wins: 0, avg: 0}
+    }))
+    .then(stats => console.log(stats))
+    .catch(console.error)
