6030eb9866bab2fe13df5a389edd540052f56cdf
Julio Serra <julioserra@Mac-mini-de-Julio.local>
2022-02-09 19:52:56 +0100

    add race game with stats and modify conditionals

--- staff/julio-serra/back-end/race-game/index.js
+++ staff/julio-serra/back-end/race-game/index.js
@@ -1,3 +1,6 @@
+const fs = require('fs').promises
+const { writeFile } = fs
+
 class Car {
     constructor(icon) {
         this.icon = icon
@@ -20,52 +23,49 @@ const car1 = new Car('🛻')
 const car2 = new Car('🚜')
 const car3 = new Car('🏎')
 
-// console.log(car1, car2, car3)
 
 const interval = setInterval(() => {
-
-    if (car1.position === 100 || car2.position === 100 || car3.position === 100) {
-        showWinner(car1, car2, car3)
-        return
-    }
-
-    car1.move(randomInteger(0, 10))
-    car2.move(randomInteger(0, 10))
-    car3.move(randomInteger(0, 10))
-
-
-    if (car1.position > 100) {
-        car1.position = 100
-        clearInterval(interval)
-    }
-    if (car2.position > 100) {
-        car2.position = 100
-        clearInterval(interval)
-    }
-    if (car3.position > 100) {
-        car3.position = 100
-        clearInterval(interval)
-    }
-
     console.clear()
     console.log(car1.toString())
     console.log(car2.toString())
     console.log(car3.toString())
+
+    if (car1.position >= 100 || car2.position >= 100 || car3.position >= 100) {
+        clearInterval(interval)
+        showResult(car1, car2, car3)
+        return
+    }
+
+    car1.move(randomInteger(0, 5))
+    car2.move(randomInteger(0, 5))
+    car3.move(randomInteger(0, 5))
+
 }, 100)
 
+
+function showResult(car1, car2, car3) {
+    const resultado = showWinner(car1, car2, car3)
+    console.log(resultado)
+
+    writeFile(`carrera-${new Date().toISOString()}.txt`, `${car1.toString()}\n${car2.toString()}\n${car3.toString()}\n${resultado}`)
+        .then(() => console.log('Carrera guardada'))
+        .catch(console.error)
+
+}
+
 function showWinner(car1, car2, car3) {
-    if (car1.position === 100 && car2.position === 100 && car3.position === 100)
-        console.log(`Empate ${car1.icon} ${car2.icon} ${car3.icon}`)
-    else if (car1.position === 100 && car2.position === 100)
-        console.log(`Empate ${car1.icon} ${car2.icon}`)
-    else if (car1.position === 100 && car3.position === 100)
-        console.log(`Empate ${car1.icon} ${car3.icon}`)
-    else if (car2.position === 100 && car3.position === 100)
-        console.log(`Empate ${car2.icon} ${car3.icon}`)
-    else if (car1.position === 100)
-        console.log(`Champion ${car1.icon}`)
-    else if (car2.position === 100)
-        console.log(`Champion ${car2.icon}`)
-    else if (car3.position === 100)
-        console.log(`Champion ${car3.icon}`)
+    if (car1.position === car2.position && car2.position === car3.position)
+        return(`Empate ${car1.icon} ${car2.icon} ${car3.icon}`)
+    else if (car1.position === car2.position && car1.position >= 100)
+        return(`Empate ${car1.icon} ${car2.icon}`)
+    else if (car1.position === car3.position && car1.position >= 100)
+        return(`Empate ${car1.icon} ${car3.icon}`)
+    else if (car2.position === car3.position && car2.position >= 100)
+        return(`Empate ${car2.icon} ${car3.icon}`)
+    else if (car1.position >= 100)
+        return(`Champion ${car1.icon}`)
+    else if (car2.position >= 100)
+        return(`Champion ${car2.icon}`)
+    else if (car3.position >= 100)
+        return(`Champion ${car3.icon}`)
 }
