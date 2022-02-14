90ba5c9cce5d005e7e2a0c29752a27c0f8c513b4
Giancarlo1516 <91284287+Giancarlo1516@users.noreply.github.com>
2022-02-14 16:35:41 +0100

    update race game

---
+++ staff/giancarlo-gonzalez/back-end/race-game/index1.js
@@ -0,0 +1,22 @@
+// class Car {
+//     constructor(icon) {
+//         this.icon = icon
+//     }
+//     toString() {
+//         return `${' '.repeat(this.position)}${this.icon}`
+//     }
+//     move(position) {
+//         this.position = position
+//     }
+// }
+// const car1 = new Car('car1')
+// const car2 = new Car('car2')
+// const car3 = new Car('car3')
+
+// car1.move(100)
+// car2.move(60)
+// car3.move(45)
+
+// console.log(car1.toString())
+// console.log(car2.toString())
+// console.log(car3.toString())
