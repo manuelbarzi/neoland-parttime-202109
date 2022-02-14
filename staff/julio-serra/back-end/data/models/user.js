5e7739fa12503e9bc85d02821a2e15006094138f
Julio Serra <julioserra@Mac-mini-de-Julio.local>
2022-02-14 20:32:37 +0100

    upload new version data with register and authenticate user

--- staff/julio-serra/back-end/data/models/user.js
+++ staff/julio-serra/back-end/data/models/user.js
@@ -1,11 +1,48 @@
+const { readFile, writeFile } = require('fs').promises
+const path = require('path')
+const { fileURLToPath } = require('url')
+
+// leo todos los usuarios
+function read() {
+    // al ser una cadena de promesas devolvemos un return 
+    const file = path.join(__dirname, './users.json')
+
+    return readFile(file, 'utf8')
+        // si va bien ".then"
+        .then(json => JSON.parse(json)) // pasamos el json(array) a objeto
+}
+
+function write(users) {
+    // al ser una cadena de promesas devolvemos un return
+    const file = path.join(__dirname, './users.json')
+    const json = JSON.stringify(users, null, 4)
+
+    return writeFile(file, json)
+}
+
 class User {
-    constructor(id, name, email, password, role) {
-        this.id = id
-        this.name = name
-        this.email = email
-        this.password = password
-        this.role = role
+    constructor(doc) {
+        this._doc = doc // _doc para decir que es un documento interno de la instancia
     }
-} 
+
+    // recibir un usuario y guardarlo en users.json
+    // convertimos el user de objeto a JSON con stringfly
+    save() {
+        // al ser una cadena de promesas devolvemos un return
+        return read()
+            .then(docs => {
+                docs.push(this._doc) // añadimos el usuario en la bbdd (que es el documento)
+
+                return write(docs)
+            })
+    }
+
+    // encontrar usuario por email
+    static findByEmail(email) {
+        return read() // Promesa de lectura, leeme todos los usuarios
+            .then(docs => docs.find(doc => doc.email === email)) // busca los usuarios que tenga el mismo email que este ultimo
+            .then(doc => doc ? new User(doc) : null)
+    }
+}
 
 module.exports = User
