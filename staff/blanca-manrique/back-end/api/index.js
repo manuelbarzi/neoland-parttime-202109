463c352cd89cd823558407aea6af6c57d4846b0c
Blanca Manrique <blancamanrique13@gmail.com>
2022-02-16 11:02:20 +0100

    data.3 contains api folder: registerUser and authenticateUser

--- staff/blanca-manrique/back-end/api/index.js
+++ staff/blanca-manrique/back-end/api/index.js
@@ -1,7 +1,33 @@
 const express = require('express')
+const { registerUser, authenticateUser } = require('logic')
 
 const api = express()
-api.get('/holamundo', (req, res) => {
-    res.send('Hola mundo')
+
+api.get('/holamundo', (req, res) => { //es un GET: no hay body que mandar al servidor
+    res.send('Hola mundo!') //si en un send no ponemos el status, por defecto status = 200
 })
+
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
+
+api.post('/api/users', jsonParser, (req, res) => {
+    //json que recibe el servidor
+    const name = req.body.name
+    const email = req.body.email
+    const password = req.body.password
+
+    try {
+        registerUser(name, email, password)
+            .then(() => {
+                res.status(201).send()
+            })
+            .catch(error => {
+                res.status(400).json({ error: error.message })
+            })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+})
+
 api.listen(8080, () => console.log('api escuchando en puerto 8080'))
+
