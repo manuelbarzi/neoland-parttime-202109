3120e5cf1a2dd99868249d3649b9170b0d1bfb4f
manuelbarzi <manuelbarzi@gmail.com>
2022-02-21 20:53:09 +0100

    add new version of data using mongoose

---
+++ staff/manuel-barzi/back-end/data/src/models/User.demo.js
@@ -0,0 +1,11 @@
+const { connect, disconnect } = require('mongoose')
+const User = require('./User')
+
+connect('mongodb://localhost:27017/demo-db')
+    .then(() => console.log('connected'))
+    // .then(() => User.findOne({ name: 'Wendy Pan'}))
+    // .then(user => console.log(user))
+    .then(() => User.find({ name: /Pan/ }))
+    .then(users => console.log(users))
+    .then(() => disconnect())
+    .then(() => console.log('disconnected'))
