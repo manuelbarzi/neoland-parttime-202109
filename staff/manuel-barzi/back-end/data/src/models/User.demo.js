4f518ef3038c23b6632a5f4792a3440905467008
manuelbarzi <manuelbarzi@gmail.com>
2022-02-21 20:57:00 +0100

    add user creation demo

--- staff/manuel-barzi/back-end/data/src/models/User.demo.js
+++ staff/manuel-barzi/back-end/data/src/models/User.demo.js
@@ -5,7 +5,15 @@ connect('mongodb://localhost:27017/demo-db')
     .then(() => console.log('connected'))
     // .then(() => User.findOne({ name: 'Wendy Pan'}))
     // .then(user => console.log(user))
-    .then(() => User.find({ name: /Pan/ }))
-    .then(users => console.log(users))
+    // .then(() => User.find({ name: /Pan/ }))
+    // .then(users => console.log(users))
+    .then(() => {
+        const user = new User({ name: 'John Doe', email: 'john@doe.com', password: '123123123' })
+
+        return user.save()
+            .then(() => {
+                console.log('john saved')
+            })
+    })
     .then(() => disconnect())
     .then(() => console.log('disconnected'))
