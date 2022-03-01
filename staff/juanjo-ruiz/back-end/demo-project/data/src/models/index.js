3eec7f9de8c7290399ec8e59bcc30417115d1d3c
Juan Jose Ruiz Suarez <juanjoruiz@MacBook-Pro.local>
2022-03-01 21:05:35 +0100

    folders organization

---
+++ staff/juanjo-ruiz/back-end/demo-project/data/src/models/index.js
@@ -0,0 +1,15 @@
+const Brand = require('./Brand')
+const Product = require('./Stock')
+const Stock = require('./Stock')
+const User = require('./User')
+const CreditCard = require('./CreditCard')
+const Order = require('./Order')
+
+module.exports = {
+    Brand,
+    Product,
+    Stock,
+    User,
+    CreditCard,
+    Order
+}
