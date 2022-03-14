cfeb3801189c91886e3c5f0fc69d62e8de327e9d
Julio Serra <julioserra@Mac-mini-de-Julio.local>
2022-03-14 20:59:11 +0100

    add update note logic

--- staff/julio-serra/back-end/notapp/logic/src/update-note.js
+++ staff/julio-serra/back-end/notapp/logic/src/update-note.js
@@ -1,7 +1,12 @@
-const { Note } = require('../../data/src/models')
+const { models: { Note } } = require('../../data')
 
-function updateNote(noteId, data, color, public, text) {
-    return Note.updateOne({ _id: noteId }, { data, color, public, text})
+function updateNote(id, noteId, color, public, text) {
+    return Note.updateOne({ user: id, _id: noteId }, { color, public, text })
+    .then(result => {
+        const { matchedCount } = result
+
+        if (matchedCount === 0) throw new Error(`note with id ${id} and user id ${id} not found`)
+    })
 }
 
 module.exports = updateNote
