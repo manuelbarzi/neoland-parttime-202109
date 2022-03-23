5ee2142b52190831788fca97c7adbecd2df3ce27
Julio Serra <julioserra@Mac-mini-de-Julio.local>
2022-03-23 23:21:28 +0100

    add react app, router dom and import tailwindCSS

---
+++ staff/julio-serra/back-end/notapp/app/src/components/Landing.js
@@ -0,0 +1,8 @@
+
+export default function Landing() {
+    return (
+    <>
+    <h1 className="text-3xl text-center">Hello Landing</h1>
+    </>
+    )
+}
--- staff/julio-serra/back-end/notapp/app/src/components/Register.js
+++ staff/julio-serra/back-end/notapp/app/src/components/Register.js
@@ -7,20 +7,26 @@ function Register() {
         const { target: { name: { value: name }, email: { value: email }, password: { value: password } } } = event //extraer los campos
         try {
             registerUser(name, email, password)
-            .then(() => alert('User Registered'))
-            .catch(error => alert(error.message))
+                .then(() => alert('User Registered'))
+                .catch(error => alert(error.message))
         } catch (error) {
             alert(error.message)
         }
     }
 
 
-    return <form style={{gap: "0.5rem", display: "flex"}} onSubmit={register}>
-        <input type="text" name="name" placeholder="name" />
-        <input type="email" name="email" placeholder="email" />
-        <input type="password" name="password" placeholder="password" />
-        <button>Submit</button>
+    return <form className="grid grid-cols-1 gap-6 justify-center flex w-1/4 mx-auto mt-20" onSubmit={register}>
+        <label>
+            <input type="text" class="mt-1 w-full" name="name" placeholder="name" />
+        </label>  
+        <label>
+            <input type="email" class="mt-1 w-full" name="email" placeholder="email" />
+        </label>      
+        <label>
+            <input type="password" class="mt-1 w-full" name="password" placeholder="password" />
+        </label>        
+        <button className='w-full bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>Submit</button>
     </form>
-} 
+}
 
 export default Register
