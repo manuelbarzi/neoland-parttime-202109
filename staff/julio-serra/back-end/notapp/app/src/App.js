25080b38b2c33d6f28fe3ce8736b08179e991f3c
Julio Serra <julioserra@Mac-mini-de-Julio.local>
2022-03-27 20:49:50 +0200

    update App file with useState

--- staff/julio-serra/back-end/notapp/app/src/App.js
+++ staff/julio-serra/back-end/notapp/app/src/App.js
@@ -3,18 +3,23 @@ import Landing from './components/Landing'
 import Login from './components/Login'
 import Home from './components/Home'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 import './index.css';
 
 
 function App() {
+
+  const { token } = sessionStorage
+  const { loggedIn, setLoggedin } = useState(!!token) //para convertirlo a Boolen lo negamos 2 veces
+  const handleLogout = () => setLoggedin(false) // ?¿
+  
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Landing />} />
+          <Route path="/" element={loggedIn ? <Home onLogout={handleLogout} /> : <Landing />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
         </Routes>
       </Router>
     </>
