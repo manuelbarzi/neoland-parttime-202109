582a9711e2ca9bfda703db66076c523c4d99fabf
Julio Serra <julioserra@Mac-mini-de-Julio.local>
2022-03-27 20:49:17 +0200

    create home component and update login component

--- staff/julio-serra/back-end/notapp/app/src/components/Home.js
+++ staff/julio-serra/back-end/notapp/app/src/components/Home.js
@@ -1,5 +1,12 @@
-export default function () {
+export default function Home({ onLogout }) {
+    const logout = () => {
+        delete sessionStorage.token
+        onLogout()
+    }
+
     return <div>
-    <h1>Hello Home !</h1>
+        <h1>Autenticación correcta</h1>
+        <h1>Bienvenid@ a Home NoteApp</h1>
+        <button onClick={logout}>Log Out</button>
     </div>
 }
--- staff/julio-serra/back-end/notapp/app/src/components/Login.js
+++ staff/julio-serra/back-end/notapp/app/src/components/Login.js
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom'
 export default function AuthenticateUser() {
 
     const navigate = useNavigate()
+    
     const auth = event => {
         event.preventDefault()
         const { target: { email: { value: email }, password: { value: password } } } = event //extraer los campos
@@ -11,7 +12,7 @@ export default function AuthenticateUser() {
             authenticateUser(email, password)
                 .then(token => {
                     sessionStorage.token = token
-                    navigate('/home')
+                    navigate('/')
                 })
                 .catch(error => alert(error.message))
         } catch (error) {
@@ -25,7 +26,7 @@ export default function AuthenticateUser() {
                 <header className="w-full container mx-auto p-6">
                     <a href="/">
                         <nav className="flex justify-between items-center">
-                            <div className="flex items-center hover:text-indigo-300 text-indigo-400 text-4xl font-bold"><svg class="h-8 fill-current text-indigo-600 pr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                            <div className="flex items-center hover:text-indigo-300 text-indigo-400 text-4xl font-bold"><svg className="h-8 fill-current text-indigo-600 pr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                 <path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm-5.6-4.29a9.95 9.95 0 0 1 11.2 0 8 8 0 1 0-11.2 0zm6.12-7.64l3.02-3.02 1.41 1.41-3.02 3.02a2 2 0 1 1-1.41-1.41z"></path>
                             </svg>AppNote</div>
                             <div className="gap-10 flex text-violet-700 text-xl">
@@ -41,14 +42,16 @@ export default function AuthenticateUser() {
                         <p className="text-2xl font-bold text-center pb-8">Sign in to NoteApp</p>
                         <form className="grid gap-6 w-1/4 mx-auto" onSubmit={auth}>
                             <label>
-                                <input className="w-full" type="email" class="mt-1 w-full" name="email" placeholder="email" />
+                                <input className="w-full" type="email" name="email" placeholder="email" />
                             </label>
                             <label className="text-right">
-                                <input className="w-full" type="password" class="mt-1 w-full" name="password" placeholder="password" />
+                                <input className="w-full" type="password" name="password" placeholder="password" />
                                 <span className="font-bold">Forgot password?</span>
                             </label>
                             <button className='w-full bg-transparent hover:bg-indigo-400 text-indigo-400 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>Login</button>
-                            <a href='/register'>Register</a>
+                            <a className="hover:underline hover:underline-offset-8 hover:text-gray-500 flex items-center gap-2" href='/register'><svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+  <path fill-rule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clip-rule="evenodd" />
+</svg> Go to Register</a>
                         </form>
                     </div>
                 </section>
