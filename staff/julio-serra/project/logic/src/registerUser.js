0f632342e0dc87ddb125238ebaaa30f407c82203
Julio Serra <julioserra@Mac-mini-de-Julio.local>
2022-05-05 20:45:59 +0200

    install bcrypt in logic, and add bcrypt in register user

--- staff/julio-serra/project/logic/src/registerUser.js
+++ staff/julio-serra/project/logic/src/registerUser.js
@@ -1,17 +1,23 @@
 const { DuplicityError } = require('../../commons/src/errors')
+const { validators: { validateName, validateEmail, validatePassword } } = require('../../commons')
 const { models: { User } } = require('data')
+const bcrypt = require('bcryptjs')
 
 function registerUser(name, email, password, image) {
+    validateName(name)
+    validateEmail(email)
+    validatePassword(password)
 
+    // return User.create({ name, email, password, image })
+    return bcrypt.hash(password, 10)
+        .then(hash => User.create({ name, email, password: hash, image }))
+        .then(user => { })
+        .catch(error => {
+            if (error.message.includes('duplicate'))
+                throw new DuplicityError('user already exists')
 
-    return User.create({ name, email, password, image })
-    .then(user => {})
-    // .catch(error => {
-    //     if(error.message.includes('duplicate'))
-    //     throw new DuplicityError('user already exists')
-
-    //     throw error
-    // })
+            throw error
+        })
 
 }
 
