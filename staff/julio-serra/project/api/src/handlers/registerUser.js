51a5f2db55805b315f5984f5b330f186a774ad21
Julio Serra <julioserra@Mac-mini-de-Julio.local>
2022-05-08 20:39:10 +0200

    update register user; create authenticate user in api; work it!

--- staff/julio-serra/project/api/src/handlers/registerUser.js
+++ staff/julio-serra/project/api/src/handlers/registerUser.js
@@ -5,18 +5,14 @@ module.exports = (req, res) => {
     try {
         const { body: { name, email, password } } = req
         registerUser(name, email, password)
-            .then(() => {
-                res.status(201).send()
-                    .catch(error => {
+            .then(() => res.status(201).send())
+            .catch(error => {
+                let status = 400
 
-                        let status = 400
+                if (error instanceof DuplicityError)
+                    status = 409
 
-                        if (error instanceof DuplicityError)
-
-                            status = 409
-
-                        res.status(400).json({ error: error.message })
-                    })
+                res.status(400).json({ error: error.message })
             })
     } catch (error) {
         res.status(400).json({ error: error.message })
