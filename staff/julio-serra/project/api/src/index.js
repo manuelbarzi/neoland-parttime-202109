0de8016879f7ee8574bcd71aad94ed188f1a1de0
Julio Serra <julioserra@Mac-mini-de-Julio.local>
2022-05-08 20:23:47 +0200

    add api files, install body-parser, cors, dotenv, nodemon, jsonwebtoken

---
+++ staff/julio-serra/project/api/src/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express')
+const { registerUser, authenticateUser } = require('./handlers')
+const { mongoose: { connect } } = require('data')
+const cors = require('cors') // para evitar el error de CORS
+const { } = require('logic')
+const { env: { MONGODB_URL, PORT } } = process
+
+connect(MONGODB_URL)
+    .then(() => {
+        console.log('connected to db')
+        const api = express()
+        api.use(cors())
+
+        const router = express.Router()
+        const jsonBodyParser = express.json()
+
+        // REGISTER USER
+        router.post('/users', jsonBodyParser, registerUser)
+
+        // AUTHENTICATE USER
+        router.post('/users/auth', jsonBodyParser, authenticateUser)
+
+        api.use('/api', router)
+        api.listen(PORT, () => console.log('json server running'))
+
+    })
