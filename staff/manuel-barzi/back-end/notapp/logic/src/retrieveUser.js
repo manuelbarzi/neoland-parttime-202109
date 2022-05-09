16df54a9eafe690f34204c6483a1ea16f55da3f1
manuelbarzi <manuelbarzi@gmail.com>
2022-05-09 20:39:47 +0200

    add retrieve user test, improve logic with it

--- staff/manuel-barzi/back-end/notapp/logic/src/retrieveUser.js
+++ staff/manuel-barzi/back-end/notapp/logic/src/retrieveUser.js
@@ -1,10 +1,13 @@
 const { models: { User } } = require('data')
+const { errors: { NotFoundError }, validators: { validateId }} = require('commons')
 
 function retrieveUser(userId) {
-    // validate arguments
+    validateId(userId, 'user id')
 
     return User.findById(userId).lean()
         .then(user =>  {
+            if (!user) throw new NotFoundError(`user with id ${userId} not found`)
+
             // sanitize
             user.id = user._id.toString()
             delete user._id
